027426f63ccbf65641edd7b64801d17b640280d3
startho <154859404+startho@users.noreply.github.com>
2024-08-08 18:44:21 +0900

    main화면 수정

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,10 +6,7 @@ import Navs from './components/Navs';
 import Login from './pages/Login';
 import Board from './pages/Board';
 import Register from './pages/Register';
-<<<<<<< HEAD
-=======
 import Stage from './pages/Stage';
->>>>>>> 2c35a209c6b5c218d8618f5ec224ea6455b5548a
 
 
 
@@ -24,10 +21,8 @@ function App() {
         <Route path="/Login" element={<Login/>}/>
         <Route path="/Board" element={<Board/>}/>
         <Route path="/Register" element={<Register/>}/>
-<<<<<<< HEAD
-=======
+
         <Route path="/Stage" element={<Stage/>}/>
->>>>>>> 2c35a209c6b5c218d8618f5ec224ea6455b5548a
       </Routes>
     </div>
   );
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -2,21 +2,16 @@ import React from 'react'
 import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 import "../css/Home.css"
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
 import WaveSection from '../components/WaveSection'; // SVG 컴포넌트 import
-=======
->>>>>>> 2c35a209c6b5c218d8618f5ec224ea6455b5548a
 
 const Home = () => {
   const nav = useNavigate();
   
   return (
-<<<<<<< HEAD
+
     <>
     <Container className="text-center">
-=======
-    <Container>
->>>>>>> 2c35a209c6b5c218d8618f5ec224ea6455b5548a
+  
         <Row>
             <Col>
             <img src='./image/ajae.png' />
@@ -24,12 +19,12 @@ const Home = () => {
             </Col>
         </Row>
         <Row> 
-<<<<<<< HEAD
             <Col className="button-container">
             <Button className="me-4 btn" >단계별</Button>
             <Button className="me-4 btn user" onClick={()=>{nav('/Board')}}>유저별</Button>
             </Col>
         </Row>
+    
     </Container>
     <Container fluid className="position-relative">
         <Row>
@@ -39,15 +34,12 @@ const Home = () => {
         </Row>
         <WaveSection /> {/* SVG 컴포넌트를 여기에 추가 */}
       </Container>
+      
+   
+   
     </>
-=======
-            <Col >
-            <Button className="me-4 btn" >단계별</Button>
-            <Button className="me-4 btn" onClick={()=>{nav('/Board')}}>유저별</Button>
-            </Col>
-        </Row>
-    </Container>
->>>>>>> 2c35a209c6b5c218d8618f5ec224ea6455b5548a
+
+
   )
 }
 
